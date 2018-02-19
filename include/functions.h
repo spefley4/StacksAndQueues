edf83b1797ef9d724c64144ce848bf7ed4831563
Seth Pefley <pefleys@eos04.cis.gvsu.edu>
2018-02-18 22:02:06 -0500

    Initial commit.

---
+++ include/functions.h
@@ -0,0 +1,14 @@
+#ifndef __H_FUNCTIONS__
+#define __H_FUNCTIONS__
+
+#include <string>
+#include <iostream>
+
+Class Functions {
+
+	bool balancedSymbols(std::string input);
+	std::string iToP(std::string input);
+
+}
+
+#endif
---
+++ src/functions.cpp
@@ -0,0 +1,16 @@
+#include "functions.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main(int argc, char** argv) {
+
+	bool balancesSymbols(std::string input) {
+		
+	}
+
+	std::string iToP(std::string input) {
+
+	}
+
+}
